4c0dd53109e87699e84c7cd95a84864b1b35e4a0
Chris Mungall <cjm@berkeleybop.org>
2018-03-08 08:22:22 -0800

    pmg support

---
+++ examples/pubmedgraph-examples.sh
@@ -0,0 +1,7 @@
+
+# ---
+# what does PMID:1 reference?
+# ---
+pq-pmg  "references(pmid:'1',X)"
+
+
---
+++ prolog/sparqlprog/ontologies/pubmedgraph.pl
@@ -0,0 +1,21 @@
+/*
+
+  ideally this is used in combination with ontobee
+  
+*/
+
+:- module(pubmedgraph,
+          [references/2,
+           coreference/3]).
+
+:- use_module(library(sparqlprog)).
+:- use_module(library(semweb/rdf11)).
+
+:- sparql_endpoint( pmg, 'https://stars-app.renci.org/pubmedgraph/sparql').
+
+:- rdf_register_prefix(dcterm,'http://purl.org/dc/terms/').
+:- rdf_register_prefix(pmid,'https://www.ncbi.nlm.nih.gov/pubmed/').
+
+references(P,T) :- rdf(P,dcterm:references,T).
+coreference(P1,P2,T) :- references(P1,T),references(P2,T).
+
