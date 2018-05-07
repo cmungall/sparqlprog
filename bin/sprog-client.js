be884871a298f3ba817a776fa5ce62b0871affc4
cmungall <cjm@berkeleybop.org>
2018-05-06 17:42:25 -0700

    fixed iteration

--- bin/sprog-client.js
+++ bin/sprog-client.js
@@ -4,7 +4,8 @@ peng = pengines({
     server: "http://127.0.0.1:9083/pengine",
     //ask: "member(X,[a,b,c,d,7])",
     ask: "t(X)",
-    chunk: 100,
+    //ask: "between(1,1000,N)",
+    chunk: 10,
     //sourceText: "t(X):- (go ?? rdf(X,rdf:type,owl:'ObjectProperty')).",
     //sourceText: "t(X):- (go ?? rdf(X,rdf:type,owl:'ObjectProperty')).\n",
     sourceText: "t(X):- (wd ?? continent(X)).\n",
@@ -18,6 +19,12 @@ function handleSuccess(result) {
     }
     if (result.data.length == 0) {
         console.log("No results!")
+    }
+    console.log('# More? '+ result.more);
+    if (result.more) {
+        n = peng.next();
+        console.log('# Next = '+ n);
+        
     }
 }
 function handleError(result) {
