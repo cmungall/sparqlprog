f659c62b3a9d4009752e6d66ef2d10873eb05b1c
cmungall <cjm@berkeleybop.org>
2018-10-01 06:56:39 +0100

    prototyping for https://github.com/oborel/obo-relations/issues/264

--- examples/ontobee-examples.sh
+++ examples/ontobee-examples.sh
@@ -11,7 +11,12 @@ pq-ontobee -u sparqlprog/ontologies/ncit  -l -f csv  "in_ontology(X,ncit,owl:'Ob
 pq-ontobee -u sparqlprog/ontologies/ncit  -f csv  "in_ontology(P,ncit,owl:'ObjectProperty'),label(P,PN),owl:domain(P,D),owl:range(P,R),label(D,DN),label(R,RN)" "x(P,PN,D,DN,R,RN)"
 
 # all property usages across ontobee (TODO: select DISTINCT)
-pq-ontobee  "rdf(_,owl:onProperty,P,G),label(P,PN)" "x(P,PN,G)"
+pq-ontobee  "rdf(Restr,owl:onProperty,P,G),label(P,PN)" "x(Restr,P,PN,G)"
+
+# similar to above, but instead load RO triples into memory, loop over all OP directly in prolog engine,
+# executing separate sparql query for each
+pq-ontobee -e -i ro.owl  "owl:objectProperty(P), (ontobee ?? rdf(Restr,owl:onProperty,P,G),label(P,PN))" "x(P,PN,G,Restr)"
+pq-ontobee -e -i ro.owl  "owl:objectProperty(P), label(P,PN), (ontobee ?? aggregate_group(count(Restr),[P,G],rdf(Restr,owl:onProperty,P,G),Num))" "x(P,PN,G,Restr)"
 
 # also:
 pq-ontobee  "aggregate_group(count(P),[P,G],rdf(_,owl:onProperty,P,G),Num)" 
