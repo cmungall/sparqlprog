e5c72679d55c4065e95e9e6857017006a04c718c
Hirokazu Chiba <hchiba1@gmail.com>
2019-09-06 09:31:43 +0900

    modify keggoc endpoint and ontology

--- prolog/sparqlprog/ontologies/keggoc.pl
+++ prolog/sparqlprog/ontologies/keggoc.pl
@@ -3,13 +3,13 @@
 	      keggoc_ortholog/2
 	  ]).
 
-:- sparql_endpoint(keggoc, 'https://keggoc-rdf.dbcls.jp/sparql/').
+:- sparql_endpoint(keggoc, 'https://www.genome.jp/oc/proxy/sparql').
 
-:- rdf_register_prefix(orth, 'http://purl.jp/bio/11/orth#').
+:- rdf_register_prefix(orth, 'http://purl.org/net/orth#').
 
 keggoc_ortholog(G1, G2) :-
-    rdf_path(OC, zeroOrMore(orth:member), G1),
-    rdf_path(OC, zeroOrMore(orth:member), G2),
+    rdf_path(OC, zeroOrMore(orth:hasHomologous), G1),
+    rdf_path(OC, zeroOrMore(orth:hasHomologous), G2),
     rdf(G1, rdf:type, orth:'Gene'),
     rdf(G2, rdf:type, orth:'Gene').
 
