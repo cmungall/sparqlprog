febf6d4d01e7e5849d9e5b10548e4614107b6fd3
cmungall <cjm@berkeleybop.org>
2019-09-06 11:11:00 +0900

    updated example of federated Q

--- examples/oma_ebi_federated.pl
+++ examples/oma_ebi_federated.pl
@@ -1,11 +1,22 @@
 
+/*
+
+  Example:
+  
+pl2sparql -e --consult examples/oma_ebi_federated.pl "orthologs_with_coordinates(ensembl:'ENSG00000198840',G2,Tax,E,R)"
+
+  
+
+    */
+
 :- use_module(library(sparqlprog/ontologies/oma)).
 :- use_module(library(sparqlprog/ontologies/ebi),[]).
 :- use_module(library(sparqlprog/ontologies/faldo)).
 
-orthologs_with_coordinates(G1,G2,B,E,R) :-
+%! orthologs_with_coordinates(?Gene1, ?Gene2, ?Taxon, ?Begin:int, End:int, ?Ref) :-
+orthologs_with_coordinates(G1,G2,T,B,E,R) :-
         (   oma ?? (is_orthologous_to(P1,P2),
-                    organism_in_taxon(P2,'http://purl.uniprot.org/taxonomy/9606'),
+                    organism_in_taxon(P2,T),
                     encoded_by_ensembl_gene(P1,G1),
                     encoded_by_ensembl_gene(P2,G2)
                     )),
