18879db4b9c5eda371dab64330a1280f7973d363
cmungall <cjm@berkeleybop.org>
2019-09-16 15:08:34 -0700

    examples

--- examples/uniprot-examples.sh
+++ examples/uniprot-examples.sh
@@ -5,12 +5,23 @@
 # ---
 # human proteins with disease and interpro
 # ---
-pq-up  "in_human(P),annotation(P,A),disease_annotation(A),interpro(P,X)
+pq-up  "in_human(P),annotation(P,A),disease_annotation(A),interpro(P,X)"
 
 # ---
 # diseases for a specific protein and their annotations
 # ---
 pq-up "protein_natural_variant_disease(uniprot:'Q15465',A,D),substitution(A,Seq)" "x(A,D,Seq)"
+
+# link from a specific protein to its variant to associated disease and the dbsnp id for the variant
+pq-up "protein_natural_variant_disease_dbsnp(uniprot:'Q15465',A,D,X)"
+
+# proteins associated with >1 disease
+pq-up -l -L pref_label  "protein_natural_variant_disease(P,V1,D1),protein_natural_variant_disease(P,V2,D2),D1\=D2"
+
+# count of all proteins
+$ pq-up "aggregate(count(P),protein(P),N)" "count(P)"
+#262095770
+
 
 # ---
 # count of xrefs broken down by database
