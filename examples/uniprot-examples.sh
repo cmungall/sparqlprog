7814483ed36cf261895dfa044a5d0da1b943d4e6
cmungall <cjm@berkeleybop.org>
2019-11-15 13:14:13 -0800

    docs

--- examples/uniprot-examples.sh
+++ examples/uniprot-examples.sh
@@ -15,6 +15,15 @@ pq-up "protein_natural_variant_disease(uniprot:'Q15465',A,D),substitution(A,Seq)
 # link from a specific protein to its variant to associated disease and the dbsnp id for the variant
 pq-up "protein_natural_variant_disease_dbsnp(uniprot:'Q15465',A,D,X)"
 
+# all xrefs
+pq-up "protein_natural_variant_disease_xref(uniprot:'Q15465',A,D,X)"
+
+# all annotations and their types on a specific protein
+pq-up  "annotation(uniprot:'Q15465',A),rdf(A,rdf:type,T)"
+
+# all sequence annotation types and their parent classification
+pq-up "rdfs_subclass_of(T,up:'Sequence_Annotation'),subClassOf(T,Parent),rdf_is_iri(Parent)"
+
 # proteins associated with >1 disease
 pq-up -l -L pref_label  "protein_natural_variant_disease(P,V1,D1),protein_natural_variant_disease(P,V2,D2),D1\=D2"
 
@@ -22,6 +31,11 @@ pq-up -l -L pref_label  "protein_natural_variant_disease(P,V1,D1),protein_natura
 $ pq-up "aggregate(count(P),protein(P),N)" "count(P)"
 #262095770
 
+
+# ---
+# distinct databases used in xrefs
+# ---
+pq-up --distinct "database(X,D)" "x(D)"
 
 # ---
 # count of xrefs broken down by database
@@ -36,6 +50,8 @@ pq-up "xref(P,X),database(X,updb:'MGI')"
 # reviewed subset
 pq-up "xref(P,X),database(X,updb:'MGI'),reviewed(P)"
 
+# xrefs to OMIM (this will be both proteins and diseases)
+pq-up -l "xref(P,X),database(X,updb:'MIM')"
 
 # ---
 # describe a specific mouse protein
